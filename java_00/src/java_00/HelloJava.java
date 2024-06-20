54017ffaad319a6484c102df24c91a6ac5dc96f0
USER <USER@1.209.180.139>
2024-06-20 09:11:21 +0900

    <2024.06.19> java 기초예제

--- java_00/src/java_00/HelloJava.java
+++ java_00/src/java_00/HelloJava.java
@@ -1,18 +1,25 @@
 package java_00;
 
+
+/**
+ * 작성자 : 이경미
+ * 작성일자 : 2024. 06. 18
+ * 코멘트 : 자바 예제 소스 코드
+ * */
+
+
 public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String test1 = "안녕하세요, 저는";
+		//문자열 예제
+		String test1 = "안녕하세요, 저는 ";
 		
 		String test2 = "입니다.";
 		
-		int i = 0;
-		
-		for(i=1; i<10; i++) {
-			System.out.println( test1 + i + test2 );
-		}
+		String name = "이경미";
+	
+		System.out.println(test1 + name + test2);
 	}
 }
