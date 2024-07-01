052dd1b0d1c63e6d61104c8dfb1931b33ab49469
USER <USER@1.209.180.139>
2024-07-01 12:32:15 +0900

    <2024.07.01> 회원가입 화면 ㅇ

---
+++ java_00/src/memberMng/MemberController.java
@@ -0,0 +1,11 @@
+package memberMng;
+
+public class MemberController {
+
+	public static void main(String[] args) {
+		MemeberService memberService = new MemeberService();
+		
+		memberService.startProgram();
+		
+	}
+}
