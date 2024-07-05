e3629fdf839c192e98fd91c4054e7d09e5ac375b
USER <USER@1.209.180.139>
2024-07-05 12:11:26 +0900

    <2024.07.05> 추상클래스,인터페이스 예제

---
+++ java_00/src/java_06/abs03/Player.java
@@ -0,0 +1,34 @@
+package java_06.abs03;
+
+public class Player {
+
+	private PlayerLevel level;
+	
+	public Player() {
+		level = new BeginnerLevel();
+		level.showLevelMessage();
+
+	}
+
+	public PlayerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(PlayerLevel level) {
+		this.level = level;
+	}
+
+
+    public void upgradeLevel(PlayerLevel level) {
+        this.level = level;
+        level.showLevelMessage();
+    }
+
+	
+    public void play(int count) {
+        this.level.go(count);
+    }
+
+	
+	
+}
